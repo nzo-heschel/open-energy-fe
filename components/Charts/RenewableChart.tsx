7965f660a9fb5cff5eeb82d8c59b86249e683f26
Sinelnikov <aktep010@gmail.com>
2026-05-26 14:57:10 +0300

    done 11

--- components/Charts/RenewableChart.tsx
+++ components/Charts/RenewableChart.tsx
@@ -371,22 +371,12 @@ export default function RenewableChart() {
           <ResponsiveContainer width="100%" height="100%">
             <ComposedChart
               data={chartData}
-              margin={{ top: 10, right: 20, left: 20, bottom: 28 }}
+              margin={{ top: 10, right: 20, left: 20, bottom: 18 }}
             >
               <CartesianGrid vertical={false} />
               <XAxis
                 dataKey="year"
                 tick={{ fontSize: 12 }}
-                label={{
-                  value: "שיעור אנרגיה מתחדשת",
-                  position: "insideBottom",
-                  offset: -8,
-                  style: {
-                    textAnchor: "middle",
-                    fontFamily: "Heebo, sans-serif",
-                    fontSize: 12,
-                  },
-                }}
               />
               <YAxis
                 tick={{ fontSize: 12 }}
